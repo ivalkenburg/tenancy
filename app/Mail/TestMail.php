8e8cabed435ea4676c504d265f911ea0068a76fc
ivalkenburg <i.valkenburg@ziggo.nl>
2020-06-30 23:43:38 +0200

    update

--- app/Mail/TestMail.php
+++ app/Mail/TestMail.php
@@ -6,9 +6,8 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Spatie\Multitenancy\Jobs\TenantAware;
 
-class TestMail extends Mailable implements ShouldQueue, TenantAware
+class TestMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 

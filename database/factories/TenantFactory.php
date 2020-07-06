8110f9ed75505b12ddd75bcecb5de67de6caa1e9
Igor Valkenburg <lolrogii@gmail.com>
2020-07-07 00:51:02 +0200

    yep

--- database/factories/TenantFactory.php
+++ database/factories/TenantFactory.php
@@ -3,19 +3,11 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Helpers\Multitenancy\Models\Tenant;
-use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Tenant::class, function (Faker $faker) {
     return [
         'name' => $faker->company,
-        'domain' => $faker->domainName,
-    ];
-});
-
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-
+        'domain' => $faker->unique()->domainName,
     ];
 });
